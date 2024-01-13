2ac79d04564dda1188e30c7f26a1349429ff7af7
washira5lab <washira@5lab.co>
2024-01-13 10:30:48 +0700

    📝 chore: add assets for socks images and styles.css ✨ feat: create index.html and main.js files for Vue app 🐛 fix: update server.ts to fix port variable case and add support for process.env.PORT

---
+++ main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'A pair of warm, fuzzy socks',
+      image: './assets/images/socks_green.jpg',
+      inventory: 100,
+      onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+      ],
+      sizes: ['S', 'M', 'L', 'XL', 'XXL']
+    }
+  }
+})      
