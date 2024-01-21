938f027aa0914c90eb67f9f32e9b55f991c6b573
washira5lab <washira@5lab.co>
2024-01-21 12:48:20 +0700

    🎨 style(index.html): add "Add to cart" button to improve user experience 🚀 feat(main.js): add cart property to track the number of items in the cart for better shopping experience

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       description: 'A pair of warm, fuzzy socks',
       image: './assets/images/socks_green.jpg',
