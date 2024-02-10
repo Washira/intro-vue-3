82fe2e16f13906376538aec286379d18585485e4
washira5lab <washira@5lab.co>
2024-02-10 09:45:46 +0700

    🔧 fix(index.html): remove unnecessary comments and reorganize code for better readability ✨ feat(index.html): update product title to use brand name and product name for better branding 🔧 fix(main.js): fix typo in brand name variable ✨ feat(main.js): add computed properties for image, inStock, and sale to improve code organization and readability

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
       description: 'A pair of warm, fuzzy socks',
       image: './assets/images/socks_green.jpg',
-      inStock: false,
-      inventory: 10,
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL']
     }
@@ -25,6 +25,23 @@ const app = Vue.createApp({
     },
     updateVariant(index) {
       this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on sale!'
+      }
+      return
     }
   }
 })      
